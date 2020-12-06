549adbda2f882941e020e6fd93f3f6a20ceb7776
hebol <martin@sjoblom.se>
2020-12-06 06:57:15 +0100

    Day 6..sigh!

--- 05/main05.js
+++ 05/main05.js
@@ -4,19 +4,19 @@ let arrayList = utils.readFile();
 console.log('Data is:', arrayList);
 
 let data = arrayList.map(entry => entry.replaceAll(/[BR]/g, '1').replaceAll(/[FL]/g, '0'));
-data = data.map(entry => {return {row: entry.substring(0,7), col: entry.substring(7)}});
-data = data.map(entry => {return {row: parseInt(entry.row, 2), col: parseInt(entry.col, 2)}});
-data = data.map(entry => entry.row * 8 + entry.col);
-console.log('Result', data, Math.max(...data));
+data = data.map(entry => parseInt(entry, 2));
 data = data.sort((a,b) => a-b);
-console.log('Result', data);
+
+let found;
 data.reduce((rest, now) => {
   if (rest === -1) {
     return now;
   } else {
     if (now > rest + 1) {
-      console.log('Found it!!', rest + 1);
+      found = rest + 1;
     }
     return now;
   }
 }, -1);
+console.log('Högsta är', Math.max(...data));
+console.log('Hittade min plats:', found);
