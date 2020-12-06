cc58e965dc12746fc1b6bc60e9f68de21643bc60
hebol <martin@sjoblom.se>
2020-12-06 07:51:55 +0100

    Created process function which made solution much simpler!

--- 06/main.js
+++ 06/main.js
@@ -1,34 +1,19 @@
 const utils = require('../utils');
-//let arrayList = utils.readFileNoFilter('test1.txt')
-//let arrayList = utils.readFileNoFilter('inputt2.txt')
-let arrayList = utils.readFileNoFilter()
 
-let current = null
-let sum = 0;
-while (arrayList.length > 0) {
-  const line = arrayList.shift();
-  if (line.length > 0) {
-    if (true) {
-      current = current || {};
-      line.split("").forEach(char => current[char] = char);
-    } else {
-      if (current) {
-        let common = Object.keys(current).filter(char => line.indexOf(char) >= 0);
-        current = common.reduce((rest, char) => {
-          rest[char] = char;
-          return rest;
-        }, {})
-      } else {
-        current = {};
-        line.split("").forEach(char => current[char] = char);
-      }
-    }
-  } else {
-    !current && console.log('Line', line);
-    if (current) {
-      sum += Object.keys(current).length;
-    }
-    current = null;
-  }
-}
-console.log('Result', sum);
+let problem1 = utils.processMultiLine(null, lines => {
+  return Object.keys(lines.reduce((rest, line) => {
+    line.split("").forEach(char => rest[char] = char);
+    return rest;
+  }, {})).length;
+});
+
+let problem2 = utils.processMultiLine(null, lines => {
+  var commonKeys = lines.shift().split("");
+  lines.forEach(line => {
+    commonKeys = commonKeys.filter(char => line.indexOf(char) >= 0);
+  })
+  return commonKeys.length;
+});
+
+console.log(problem1.reduce((rest, entry) => rest + entry, 0));
+console.log(problem2.reduce((rest, entry) => rest + entry, 0));
--- utils.js
+++ utils.js
@@ -12,5 +12,23 @@ module.exports = {
     const stringData = new String(rawData);
     var arrayList = stringData.split('\n');
     return arrayList;
+  },
+  processMultiLine: (name, handler) => {
+    const rawData = fs.readFileSync(name || './input.txt');
+    const stringData = new String(rawData);
+    var arrayList = stringData.split('\n');
+    let sum = [];
+    let currentData = [];
+    while (arrayList.length > 0) {
+      const line = arrayList.shift();
+      if (line.length > 0) {
+        currentData.push(line);
+      }
+      if (line.length === 0 || arrayList.length === 0) {
+        sum.push(handler(currentData));
+        currentData = [];
+      }
+    }
+    return sum;
   }
 };
