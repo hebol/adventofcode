25211bc730aa1e43a1d8f06c98a2e12fb1b941d4
hebol <martin@sjoblom.se>
2020-12-08 06:07:29 +0100

    Added testdata

--- 08/main08.js
+++ 08/main08.js
@@ -1,37 +1,12 @@
 const utils = require('../utils');
 
-const bags = {
+const visitedLines = {
 };
 
 utils.processLine(line => {
-  const match = line.match(/^([a-z]{3}) ([+\-][0-9]+)$/)
-})
+  const match = line.match(/^([a-z]{3}) ([+\-][0-9]+)$/);
+  console.log(match)
+}, 'test1.txt');
 
-const containGoldDirectly = Object.values(bags.colors).filter(entry => {
-  return Object.values(entry.children).filter(entry => entry.color === 'shiny gold').length > 0;
-})
-containGoldDirectly.forEach(entry => entry.gold = true);
-let modified = true;
-do {
-  modified = false;
-  Object.values(bags.colors).forEach(entry => {
-    if (Object.values(entry.children).filter(subEntry => subEntry.gold).length > 0 && !entry.gold) {
-      entry.gold = true;
-      modified = true;
-    }
-  })
-} while (modified)
-
-const answer1 = Object.values(bags.colors).filter(entry => entry.gold);
-
-//144
-console.log('Bags that can contain "shiny gold"', answer1.length);
-
-const processBag = (aBag) => {
-  return 1 + aBag.childrenRaw.reduce((rest, subBag) => {
-    return rest + subBag.count * processBag(bags.colors[subBag.color]);
-  }, 0)
-}
-
-// 5956
-console.log('Bag count in Shiny gold', processBag(bags.colors['shiny gold']) - 1);
+//const answer1 = Object.values(bags.colors).filter(entry => entry.gold);
+//console.log('Bag count in Shiny gold', processBag(bags.colors['shiny gold']) - 1);
