421886f3ced16dd5df4fc2daa79ed17dff7a5af3
hebol <cmsgb0523>
2020-12-09 08:17:06 +0100

    Något uppsnyggad

--- 09/main09.js
+++ 09/main09.js
@@ -1,26 +1,23 @@
 const utils = require('../utils');
 
-const data = {
-  lines: []
-};
+const lines = [];
 
 utils.processLine(line => {
-  data.lines.push(parseInt(line));
+  lines.push(parseInt(line));
 }, 'input.txt');
 
-console.log('There are', data.lines.length, 'rows');
+console.log('There are', lines.length, 'rows');
 
 function findPair(array, target) {
-  let result = false;
   return array.filter(number => array.indexOf(target - number) >= 0 && 2*number !== target).length;
 }
 
 //Hittade inget par för  258585477
 const process = preamble => {
-  for (var i = preamble ; i < data.lines.length ; i++) {
-    if (!findPair(data.lines.slice(i - preamble, i), data.lines[i])) {
-      console.log('Hittade inget par för ', data.lines[i]);
-      return data.lines[i];
+  for (var i = preamble ; i < lines.length ; i++) {
+    if (!findPair(lines.slice(i - preamble, i), lines[i])) {
+      console.log('Hittade inget par för ', lines[i]);
+      return lines[i];
     }
   }
 }
@@ -28,12 +25,12 @@ const process = preamble => {
 const sum = process(25);
 
 function findSum(aSum) {
-  for (var i = 0 ; i < data.lines.length - 1 ; i++) {
+  for (var i = 0 ; i < lines.length - 1 ; i++) {
     let currentSum = 0;
-    for (var j = i ; j < data.lines.length ; j++) {
-      currentSum += data.lines[j];
+    for (var j = i ; j < lines.length ; j++) {
+      currentSum += lines[j];
       if (currentSum === aSum) {
-        let solution = data.lines.slice(i, j + 1);
+        let solution = lines.slice(i, j + 1);
         const min = Math.min(...solution);
         const max = Math.max(...solution);
         console.log('Lösning 2 hittad', solution, {min, max});
