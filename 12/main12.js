25e85786e9b3c67dff82eec1d6001006bfcf1eb8
hebol <martin@sjoblom.se>
2020-12-12 06:05:37 +0100

    With data

--- 12/main12.js
+++ 12/main12.js
@@ -1,1 +1,13 @@
 const utils = require('../utils');
+
+let direction = 'E';
+let x = 0;
+let y = 0;
+
+utils.processLine(line => {
+  const match = line.match(/([A-Z]])([0-9]+)/)
+
+  console.log(match[1], match[2]);
+}, 'test1.txt');
+
+console.log('Distance is', x+y);
