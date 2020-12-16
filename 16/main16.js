b415629b4694285384bab9375e329416826d22d0
hebol <cmsgb0523>
2020-12-16 07:48:54 +0100

    Start solution

--- 16/main16.js
+++ 16/main16.js
@@ -1,1 +1,14 @@
 const utils = require('../utils');
+
+const fields = {}
+
+let array = utils.readFile('test1.txt');
+let rules = array.slice(0, array.indexOf('your ticket:'));
+const yourTicket = array[array.indexOf('your ticket:') + 1];
+const nearby = array.slice(array.indexOf('your ticket:') + 3);
+rules.forEach(line => {
+  const match = line.match(/([a-z]+): (\d+)-(\d+) or (\d+)-(\d+)/);
+  console.log(match);
+})
+
+console.log({rules, yourTicket, nearby});
