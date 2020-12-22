45f837d9e64559f48bfffa2a9029f0684e9bd70c
hebol <martin@sjoblom.se>
2020-12-22 06:20:26 +0100

    part1 ready

--- 22/main22.js
+++ 22/main22.js
@@ -1,1 +1,23 @@
 const utils = require('../utils');
+
+const hands = utils.processMultiLine(lines => {
+  return {
+    name: lines.shift(),
+    cards: lines.map(entry => parseInt(entry))
+  };
+}, 'input.txt')
+console.log(hands);
+
+while (hands[0].cards.length > 0 && hands[1].cards.length > 0) {
+  const card1 = hands[0].cards.shift();
+  const card2 = hands[1].cards.shift();
+  if (card1 > card2) {
+    hands[0].cards = hands[0].cards.concat([card1, card2])
+  } else {
+    hands[1].cards = hands[1].cards.concat([card2, card1])
+  }
+}
+const winner = hands[0].cards.length > 0 ? hands[0] : hands[1];
+let index = 0;
+const answer1 = winner.cards.reverse().reduce((rest, part) => rest + part * ++index, 0);
+console.log({answer1});
