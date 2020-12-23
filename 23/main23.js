2ac0881f477c39b1b1a280f135386b1ccbe081bb
hebol <martin@sjoblom.se>
2020-12-23 13:49:58 +0100

    Part2 ready keep on linking!

--- 23/main23.js
+++ 23/main23.js
@@ -2,31 +2,86 @@ const utils = require('../utils');
 
 const [orgData] = utils.readFile('input.txt');
 
-let state = orgData.split('').map(entry => parseInt(entry));
+function pickup(node) {
+  const result = node.next;
+  node.next = node.next.next.next.next
+  result.next.next.next = null;
+  return result;
+}
 
-for (let i = 0 ; i < 100 ; i++) {
-  const pickedUp = state.splice(1, 3);
-  let current = state[0];
-  let destNumber = current - 1;
-  let targetIndex;
-  //console.log('Before', {current:current, pickedUp, state:state.join('')});
-  while (!targetIndex || targetIndex < 0) {
-    if (destNumber === 0) {
-      destNumber = Math.max(...state);
-    }
-    targetIndex = state.indexOf(destNumber);
-    if (targetIndex < 0) {
-      destNumber -= 1;
+function isIn(num, node) {
+  while (node) {
+    if (node.num === num) {
+      return true;
+    } else {
+      node = node.next;
     }
   }
-  state = state.slice(0, targetIndex + 1).concat(pickedUp).concat(state.slice(targetIndex + 1));
-  state = state.slice(1).concat([current]);
-  const nextIndex = (state.indexOf(current) + 1 % state.length);
-  state = state.slice(nextIndex).concat(state.slice(0, nextIndex));
-  //console.log('After', {state:state.join('')});
+  return false;
 }
-const oneIndex = state.indexOf(1);
-const lastState = state.slice(oneIndex).concat(state.slice(0, oneIndex));
-const answer1 = lastState.slice(1).join('');
+
+function linkAfter(node, list) {
+  const temp = node.next;
+  node.next = list;
+  let currentNode = list;
+  while (currentNode.next) {
+    currentNode = currentNode.next;
+  }
+  currentNode.next = temp;
+}
+
+function nodeToString(node) {
+  let currentNode = node;
+  let result = '';
+  while (currentNode && currentNode.next !== node) {
+    result += currentNode.num;
+    currentNode = currentNode.next;
+  }
+  return result;
+}
+
+function simulateCrabs(state, rounds, maxNumber) {
+  const indexMap = state.reduce((rest, part) => {
+    rest[part] = {num:part};
+    return rest;
+  },{})
+  for (let i = 0 ; i < state.length ; i++) {
+    indexMap[state[i]].next = indexMap[state[(i + 1) % state.length]];
+  }
+
+  let current = indexMap[state[0]];
+  for (let i = 0; i < rounds; i++) {
+    if (i % 100000 === 0) {
+      console.log('Ping', i, new Date());
+    }
+    const pickedUp = pickup(current);
+    //console.log({state: nodeToString(current), selected: current.num, pickedUp: nodeToString(pickedUp)});
+
+    let destNumber = current.num - 1;
+    while (destNumber < 1 || isIn(destNumber, pickedUp)) {
+      if (destNumber < 1) {
+        destNumber = maxNumber;
+      } else {
+        destNumber -= 1;
+      }
+    }
+    let target = indexMap[destNumber];
+    linkAfter(target, pickedUp);
+
+    current = current.next;
+  }
+  return indexMap[1].next;
+}
+
+let result = simulateCrabs(orgData.split('').map(entry => parseInt(entry)), 100, 9);
+
+let answer1 = nodeToString(result).substring(0, 8);
 console.log({answer1});
 
+let newState = orgData.split('').map(entry => parseInt(entry));
+let bigArray = [...Array(1000001).keys()];
+bigArray.splice(0, 1);
+newState = newState.concat(bigArray.slice(newState.length))
+const result2 = simulateCrabs(newState, 10000000, newState.length);
+const answer2 = result2.num *  result2.next.num;
+console.log({answer2, result2});
