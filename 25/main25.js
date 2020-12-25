fa176f71c7e7696b7398606a41b7fd1748560651
hebol <martin@sjoblom.se>
2020-12-25 06:52:48 +0100

    Day 25 ready

--- 25/main25.js
+++ 25/main25.js
@@ -1,5 +1,31 @@
 const utils = require('../utils');
 
-const [cardPub, doorPub] = utils.readFile('test1.txt').map(entry => parseInt(entry));
+const [cardPub, doorPub] = utils.readFile('input.txt').map(entry => parseInt(entry));
 
 console.log({cardPub, doorPub});
+
+
+function calcLoopCount(pub) {
+  let subjNum = 1;
+  let count = 0;
+  while (subjNum !== pub) {
+    subjNum = (subjNum * 7) % 20201227;
+    count++;
+  }
+  return count;
+}
+
+let cardCount = calcLoopCount(cardPub);
+let doorCount = calcLoopCount(doorPub);
+
+console.log({cardCount, doorCount});
+
+const calcEnc = (num, loop) => {
+  let result = 1;
+  for (let count = 0 ; count < loop ; count++) {
+    result = (result * num) % 20201227;
+  }
+  return result;
+}
+const encryption = calcEnc(cardPub, doorCount);
+console.log({encryption});
