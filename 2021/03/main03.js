28b4d2ba60610f0b56103e1ee6cca94328fed3d1
hebol <martin@sjoblom.se>
2021-12-03 10:55:06 +0100

    Prepare for tomorrow

--- 2021/03/main03.js
+++ 2021/03/main03.js
@@ -14,18 +14,19 @@ const getFactors = ((anArray, evenValue) => {
 const {gamma, epsilon} = getFactors(arrayList, null);
 const answer1 = parseInt(gamma, 2) * parseInt(epsilon, 2);
 
-let temp1 = [...arrayList];
+let tempArray = [...arrayList];
 for (let index = 0 ; index < arrayList[0].length ; index++) {
-    const target = getFactors(temp1, "1");
-    temp1 = temp1.filter(entry => entry[index] === target.gamma[index]);
+    const target = getFactors(tempArray, "1");
+    tempArray = tempArray.filter(entry => entry[index] === target.gamma[index]);
 }
-let answer21 = temp1[0];
-temp1 = [...arrayList];
-for (let index = 0 ; index < arrayList[0].length && temp1.length > 1 ; index++) {
-    const target = getFactors(temp1, "0");
-    temp1 = temp1.filter(entry => entry[index] === target.epsilon[index]);
+let answer21 = tempArray[0];
+tempArray = [...arrayList];
+for (let index = 0 ; index < arrayList[0].length && tempArray.length > 1 ; index++) {
+    const target = getFactors(tempArray, "0");
+    tempArray = tempArray.filter(entry => entry[index] === target.epsilon[index]);
 }
-let answer22 = temp1[0];
+let answer22 = tempArray[0];
 const answer2 = parseInt(answer21, 2) * parseInt(answer22, 2);
+
 console.log({answer1, answer2});
 // { answer1: 775304, answer2: 1370737 }
