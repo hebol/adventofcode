fb8d2e2a4cfd8a8cc46afee578599d6bc502dbed
Martin Sjöblom <martin@sjoblom.se>
2021-12-04 12:21:21 +0100

    prepare for tomorrow

---
+++ 2021/05/main05.js
@@ -0,0 +1,9 @@
+const utils = require('../../utils');
+//let arrayList = utils.readFile('input.txt')
+utils.checkDataInputFileExists();
+
+let answer1, answer2;
+// Answer1:  Answer2
+
+
+console.log("Answer1:", answer1, "Answer2", answer2);
