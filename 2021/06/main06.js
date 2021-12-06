268781f26af2387aa04a86e1dd863ff3b41e01ba
Martin Sjöblom <martin@sjoblom.se>
2021-12-06 07:13:36 +0100

    Day 6 ready, preparing for new day

--- 2021/06/main06.js
+++ 2021/06/main06.js
@@ -1,9 +1,24 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('input.txt')
-utils.checkDataInputFileExists();
+let dayFishList = utils.readFile('input.txt')[0].split(',').reduce((rest,day) => {
+    rest[day] += 1;
+    return rest;
+}, Array(9).fill(0))
 
 let answer1, answer2;
-// Answer1:  Answer2
 
+function processListForDays(array, days) {
+    const anArray = [...array];
+    for (let day = 0; day < days; day++) {
+        const birthers = anArray.shift();
+        anArray[6] += birthers;
+        anArray.push(birthers);
+    }
+    return utils.sumArray(anArray);
+}
+
+answer1 = processListForDays(dayFishList, 80);
+answer2 = processListForDays(dayFishList, 256);
+
+// Answer1: 366057 Answer2 1653559299811
 
 console.log("Answer1:", answer1, "Answer2", answer2);
