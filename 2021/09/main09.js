e1147624dd56b6541a52b1c6704f6078e85229b0
hebol <martin@sjoblom.se>
2021-12-09 06:57:52 +0100

    Day 9 ready, preparing for new day

--- 2021/09/main09.js
+++ 2021/09/main09.js
@@ -1,8 +1,67 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let arrayList = utils.readFile('input.txt')
+
+const surrounding = (row, col, array) => {
+  const result = [];
+  (row > 0)                   && result.push(array[row-1][col]);
+  (row < array.length - 1)    && result.push(array[row+1][col]);
+  (col > 0)                   && result.push(array[row][col-1]);
+  (col < array[0].length - 1) && result.push(array[row][col+1]);
+
+  return result;
+}
+
+const surroundingNotNine = (row, col, array) => {
+  const result = [];
+  (row > 0)                   && array[row-1][col] !== '9' && result.push({row:row-1, col});
+  (row < array.length - 1)    && array[row+1][col] !== '9' && result.push({row:row+1, col});
+  (col > 0)                   && array[row][col-1] !== '9' && result.push({row:row, col:col-1});
+  (col < array[0].length - 1) && array[row][col+1] !== '9' && result.push({row:row, col:col+1});
+  return result;
+}
+
+function isLowPoint(array, row, col) {
+  return array[row][col] < Math.min(...surrounding(row, col, array));
+}
+
+const findLowestPoints = array => {
+  const result = [];
+  for (let row = 0 ; row < array.length ; row++) {
+    for (let col = 0 ; col < array[0].length ; col++) {
+      if (isLowPoint(array, row, col)) {
+        result.push({row, col});
+      }
+    }
+  }
+  return result;
+}
 
 let answer1, answer2;
 
+answer1 = utils.sumArray(findLowestPoints(arrayList).map(({row,col})=>parseInt(arrayList[row][col])+1));
+
+const findBasins = (points,array) => {
+  function pointToCol(point) {
+    return point.row + '_' + point.col;
+  }
+
+  const result = points.map(point => {
+    const basin = {};
+    basin[pointToCol(point)] = 1;
+    let newPoints = surroundingNotNine(point.row, point.col, array);
+    while (newPoints.length > 0) {
+      const testPoint = newPoints.pop();
+      if (!basin[pointToCol(testPoint)]) {
+        basin[pointToCol(testPoint)] = 1;
+        newPoints = newPoints.concat(surroundingNotNine(testPoint.row, testPoint.col, array));
+      }
+    }
+    return Object.keys(basin).length;
+  });
+  return result.sort((a,b)=>a-b).splice(-3).reduce((rest,part)=>rest*part, 1);
+}
+
+answer2 = findBasins(findLowestPoints(arrayList), arrayList);
+
 console.log("Answer1:", answer1, "Answer2", answer2);
-// Answer1:  Answer2
+// Answer1: 577 Answer2 1069200
---
+++ 2021/10/main10.js
@@ -0,0 +1,8 @@
+const utils = require('../../utils');
+//let arrayList = utils.readFile('test.txt')
+utils.checkDataInputFileExists();
+
+let answer1, answer2;
+
+console.log("Answer1:", answer1, "Answer2", answer2);
+// Answer1:  Answer2
