66bed56cf31b174b4a2ca3eb4238060267d16c6b
Martin Sjöblom <martin@sjoblom.se>
2021-12-11 06:03:53 +0100

    start of 11

--- 2021/11/main11.js
+++ 2021/11/main11.js
@@ -1,8 +1,10 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let arrayList = utils.readFile('test.txt').map(x=>x.split(''))
 
-let answer1, answer2;
+
+
+
+let answer1 = -1, answer2 = -1;
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1:  Answer2
--- 2021/mainX.js
+++ 2021/mainX.js
@@ -2,7 +2,7 @@ const utils = require('../../utils');
 //let arrayList = utils.readFile('test.txt')
 utils.checkDataInputFileExists();
 
-let answer1, answer2;
+let answer1 = -1, answer2 = -1;
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1:  Answer2
