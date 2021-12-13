f704572bb8808d9c2e35b94a2161fde63688bf70
Martin Sjöblom <martin@sjoblom.se>
2021-12-13 09:05:27 +0100

    Ready 13 and prepare for next day

--- 2021/12/main12.js
+++ 2021/12/main12.js
@@ -1,8 +1,57 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let arrayList = utils.readFile('input.txt')
 
 let answer1 = -1, answer2 = -1;
 
+const smallCaves = {}
+const map = {}
+
+function isSmallCave(node) {
+    return !node.split('').find(c=>c>='A'&&c<='Z') && node.length < 3;
+}
+
+arrayList.forEach(line => {
+    const [from,to] = line.split('-');
+    const current = map[from] || {};
+    current[to] = from;
+    map[from] = current;
+    const current2 = map[to] || {};
+    current2[from] = to;
+    map[to] = current2;
+    if (isSmallCave(from)) {
+        smallCaves[from] = 0;
+    }
+    if (isSmallCave(to)) {
+        smallCaves[to] = 0;
+    }
+});
+
+const findCombinations = (from, map, smallVisited, allowTwo) => {
+    const combinations = [];
+    const mySmallVisited = {...smallVisited};
+    if (isSmallCave(from)) {
+        mySmallVisited[from] += 1;
+    }
+    const myAllowTwo = allowTwo && (mySmallVisited[from] <= 1 || !isSmallCave(from));
+    const available = Object.keys(map[from]).filter(node=> {
+        const count = mySmallVisited[node];
+        return (!isSmallCave(node) || count === 0
+            || (count === 1 && myAllowTwo === true))
+            && node.localeCompare('start') !== 0;
+    });
+    available.forEach(node => {
+        if (node.localeCompare('end') !== 0) {
+            const combList = findCombinations(node, map, mySmallVisited, myAllowTwo).map(combo => node + ',' + combo);
+            combinations.push.apply(combinations, combList);
+        } else {
+            combinations.push(node);
+        }
+    });
+    return combinations;
+}
+
+answer1 = findCombinations('start', map, smallCaves, false).length;
+answer2 = findCombinations('start', map, smallCaves, true).length;
+
 console.log("Answer1:", answer1, "Answer2:", answer2);
-// Answer1:  Answer2
+// Answer1: 3485 Answer2: 85062
--- 2021/13/main13.js
+++ 2021/13/main13.js
@@ -1,57 +1,77 @@
 const utils = require('../../utils');
 
-let arrayList = utils.readFileNoFilter('test.txt');
+let arrayList = utils.readFileNoFilter('input.txt');
 
 let answer1 = -1, answer2 = -1;
 
-console.log(arrayList)
-const processList = array => {
-  let dots = [];
-  const folds = [];
-  let findFolds = false;
-  let width = 0, height = 0;
-
-  function addWidth(anArray, width) {
-    return anArray.map(part => {
-      if (part.length < width) {
-        const newPart = part.concat(Array(width - part.length).fill(' '));
-        console.log('Added width', width - part.length)
-        part = newPart;
-      }
-      return part;
-    })
-  }
-
-  for (let i = 0 ; i < array.length ; i++) {
-    const line = array[i];
-    if (line.length === 0) {
-      findFolds = true;
-      continue;
-    }
-    if (findFolds) {
-      const data = line.match(/fold along ([xy])=(\d+)/);
-      folds.push({dir:data[1],row:parseInt(data[2])});
-    } else {
-      const [x,y] = line.split(',');
-      console.log({x,y,w:dots.length ? dots[0].length : 0,h:dots.length});
-      if (dots.length && x >= dots[0].length) {
-        dots = addWidth(dots, parseInt(x) + 1);
-      }
-      if (y >= dots.length) {
-        console.log('Added height', parseInt(y) - dots.length + 1)
-        dots = dots.concat(Array(parseInt(y) - dots.length + 1).fill(' '));
-        console.log({w:dots[0].length, x:parseInt(x) + 1})
-        dots = addWidth(dots, Math.max(dots[0].length, parseInt(x) + 1));
-      }
-      console.log('Will set', y, x, 'to #', dots, line, i, {w:dots[0].length,h:dots.length, row0:dots[0]});
-      dots[y][x] = '#';
-      console.log({dots});
-    }
-  }
-  console.log({dots, folds})
+const displayGrid = grid => {
+  grid.forEach(line => {
+    console.log(line.map(c => c === '#' ? '#' : '.').join(''));
+  })
+  console.log('');
 }
 
-processList(arrayList);
+function foldRowAt(row, pos) {
+  const result = JSON.parse(JSON.stringify(row.slice(0, pos - 1)));
+  for (let i = 0 ; i < pos ; i++) {
+    const newChar = row[i] === '#' || row[row.length - i - 1] === '#' ? '#' : '.';
+    result[i] = newChar;
+  }
+  return result;
+}
+
+function foldAt(array, pos) {
+  const result = JSON.parse(JSON.stringify(array.slice(0, pos)));
+
+  for (let row = 0 ; row < array.length - pos - 1 ; row++) {
+    for (let c = 0 ; c < array[row].length ; c++) {
+      const newChar = array[row][c] === '#' || array[array.length - row - 1][c] === '#' ? '#' : '.';
+      result[row][c] = newChar;
+    }
+  }
+  return result;
+}
+
+const processList = (array,foldCount) => {
+  const folds    = [];
+  const startPos = [];
+  let findFolds  = false;
+
+  array.forEach(line => {
+    if (line.length === 0) {
+      findFolds = true;
+    } else {
+      if (findFolds) {
+        const data = line.match(/fold along ([xy])=(\d+)/);
+        folds.push({dir:data[1],pos:parseInt(data[2])});
+      } else {
+        const [x,y] = line.split(',');
+        startPos.push({x,y});
+      }
+    }
+  });
+
+  const rows = startPos.reduce((rest,{x,y}) => Math.max(y,rest), 0) + 1;
+  const cols = startPos.reduce((rest,{x,y}) => Math.max(x,rest), 0) + 1;
+  let map = new Array(rows);
+  for(let r = 0 ; r < rows ; r++) {
+    map[r] = new Array(cols).fill('.');
+  }
+  startPos.forEach(pos => map[pos.y][pos.x] = '#');
+
+  folds.slice(0, foldCount).forEach(fold => {
+    if (fold.dir === 'x') {
+      map = map.map(row => foldRowAt(row, fold.pos));
+    } else {
+      map = foldAt(map, fold.pos);
+    }
+  });
+  !foldCount && displayGrid(map);
+  return map.reduce((rest,line) => rest + line.filter(c => c === '#').length, 0)
+}
+
+answer1 = processList(arrayList, 1);
+answer2 = processList(arrayList);
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1:  Answer2
