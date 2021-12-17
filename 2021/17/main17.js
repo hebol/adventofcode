48a2339f8f09d41dbd709046a49e4fa8c690fe88
Martin Sjöblom <martin@sjoblom.se>
2021-12-17 07:23:04 +0100

    Ready day and prepare for tomorrow

--- 2021/17/main17.js
+++ 2021/17/main17.js
@@ -1,8 +1,41 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let [,fromX, toX, fromY, toY] = utils.readFile('input.txt')[0]
+    .match(/target area: x=(\d+)..(\d+), y=(-\d+)..(-\d+)/).map(x=>parseInt(x))
 
-let answer1 = -1, answer2 = -1;
+let answer1, answer2;
+
+const simulateShot = (dx, dy) => {
+    let x = 0, y = 0, maxY = 0;
+    while (x < toX && y > fromY) {
+        x += dx;
+        y += dy;
+        if (dx > 0) {
+            dx -= 1;
+        }
+        dy -= 1;
+        maxY = Math.max(maxY, y);
+        if (x >= fromX && x<= toX && y >= fromY && y <= toY) {
+            return maxY;
+        }
+    }
+    return undefined;
+}
+
+let bestX, bestY, maxY, solutions = [];
+for(let x = 0 ; x <= toX ; x++) {
+    for(let y = fromY ; y <= 200 ; y++) {
+        let resY = simulateShot(x, y);
+        resY !== undefined && solutions.push({x, y});
+        if (!maxY || maxY < resY) {
+            bestX = x;
+            bestY = y;
+            maxY = resY;
+        }
+    }
+}
+
+answer1 = maxY;
+answer2 = solutions.length;
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
-// Answer1:  Answer2
+// Answer1: 2278 Answer2: 996
