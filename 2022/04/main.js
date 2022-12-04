f2576a46e0101a58478fb8feebf5471a49628e3f
hebol <martin@sjoblom.se>
2022-12-04 06:27:13 +0100

    Ready 4

--- 2022/04/main.js
+++ 2022/04/main.js
@@ -1,8 +1,17 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let arrayList = utils.readFile('input.txt')
 
-let answer1 = -1, answer2 = -1;
+const answer1 = arrayList.filter(line => {
+  const [[start1, end1], [start2, end2]] = line.split(',').map(pair => pair.split('-').map(n => parseInt(n)));
+
+  return (start1 >= start2 && end1 <= end2) || (start2 >= start1 && end2 <= end1);
+}).length;
+
+const answer2 = arrayList.filter(line => {
+  const [[start1, end1], [start2, end2]] = line.split(',').map(pair => pair.split('-').map(n => parseInt(n)));
+
+  return (start2 <= end1  && end2 >= start1) || (start1 <= end2  && end1 >= start2);
+}).length;
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
-// Answer1:  Answer2
+// Answer1: 567 Answer2: 907
