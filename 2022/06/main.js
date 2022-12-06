bc7a00d4804ef593f219bdd597860f066442099c
hebol <martin@sjoblom.se>
2022-12-06 06:24:12 +0100

    REady 6 and prepare for 7

--- 2022/06/main.js
+++ 2022/06/main.js
@@ -1,8 +1,25 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let message = utils.readFile('input.txt')[0];
 
 let answer1 = -1, answer2 = -1;
 
+function findUniqueLenFrom(len, start) {
+  for (let i = start; i < message.length - len; i++) {
+    let found = true;
+    for (let j = 0; j < len; j++) {
+      if (message.substring(i, i + len).indexOf(message[i + j]) !== j) {
+        found = false;
+        break;
+      }
+    }
+    if (found) {
+      return i + len;
+    }
+  }
+}
+
+answer1 = findUniqueLenFrom(4,3);
+answer2 = findUniqueLenFrom(14, answer1 + 4);
+
 console.log("Answer1:", answer1, "Answer2:", answer2);
-// Answer1:  Answer2
+// Answer1: 1042 Answer2: 2980
