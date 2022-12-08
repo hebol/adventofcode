956df01657613c8e2a53cdf77fd724dd6b461a4f
hebol <martin@sjoblom.se>
2022-12-08 08:51:00 +0100

    8 cleanup

--- 2022/08/main.js
+++ 2022/08/main.js
@@ -2,14 +2,13 @@ const utils = require('../../utils');
 let heightMap = utils.readFile('input.txt').map(line => line.split('').map(char => parseInt(char)));
 let height = heightMap.length;
 let width = heightMap[0].length;
-let answer1 = -1, answer2 = -1;
+let answer1, answer2 = -1;
 
 const visible = {};
+const directions = [[0,1], [1,0], [0,-1], [-1,0]];
 
-function isVisible(orgRow, orgCol, [dirRow, dirCol]) {
-  const treeHeight = heightMap[orgRow][orgCol];
-  let row = orgRow;
-  let col = orgCol;
+function isVisible(row, col, [dirRow, dirCol]) {
+  const treeHeight = heightMap[row][col];
   while (true) {
     row += dirRow;
     col += dirCol;
@@ -22,22 +21,8 @@ function isVisible(orgRow, orgCol, [dirRow, dirCol]) {
   }
 }
 
-for (let row = 1 ; row < heightMap.length - 1 ; row++) {
-  for (let col = 1 ; col < heightMap[0].length - 1 ; col++) {
-    if (isVisible(row, col,  [0, 1]) ||
-      isVisible(row, col,  [1, 0]) ||
-      isVisible(row, col,  [-1, 0]) ||
-      isVisible(row, col,  [0, -1])) {
-      visible[row+'-'+col] = true;
-    }
-  }
-}
-answer1 = Object.keys(visible).length + 2 * height + 2 * width - 4;
-
-function viewLength(orgRow, orgCol, [dirRow, dirCol]) {
-  const treeHeight = heightMap[orgRow][orgCol];
-  let row = orgRow;
-  let col = orgCol;
+function viewLength(row, col, [dirRow, dirCol]) {
+  const treeHeight = heightMap[row][col];
   let steps = 0;
   while (true) {
     row += dirRow;
@@ -52,17 +37,17 @@ function viewLength(orgRow, orgCol, [dirRow, dirCol]) {
   }
 }
 
+
 for (let row = 1 ; row < heightMap.length - 1 ; row++) {
   for (let col = 1 ; col < heightMap[0].length - 1 ; col++) {
-    const score =
-      viewLength(row, col, [0, 1]) *
-      viewLength(row, col, [1, 0]) *
-      viewLength(row, col, [-1, 0]) *
-      viewLength(row, col, [0, -1]);
-    answer2 = Math.max(answer2, score);
+    if (directions.find(dir => isVisible(row, col, dir))) {
+      visible[row+'-'+col] = true;
+      const score = directions.reduce((acc, dir) => acc * viewLength(row, col, dir), 1);
+      answer2 = Math.max(answer2, score);
+    }
   }
 }
-
+answer1 = Object.keys(visible).length + 2 * height + 2 * width - 4;
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1: 1693 Answer2: 422059
