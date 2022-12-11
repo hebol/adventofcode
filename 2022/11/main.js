579dfdc024e2d5aee79a2bb4b2dce2180afbec6f
hebol <martin@sjoblom.se>
2022-12-11 10:39:46 +0100

    Cleanup

--- 2022/11/main.js
+++ 2022/11/main.js
@@ -2,24 +2,20 @@ const utils = require('../../utils');
 
 const monkeys = [];
 utils.processMultiLine(lines => {
-  const items = lines[1].split(':')[1].split(',').map(item => parseInt(item.trim()));
+  const orgItems = lines[1].split(':')[1].split(',').map(item => parseInt(item.trim()));
   const operation = lines[2].substring(19);
-  const test = parseInt(lines[3].substring(20));
-  const isTrue = parseInt(lines[4].substring(28));
+  const test    = parseInt(lines[3].substring(20));
+  const isTrue  = parseInt(lines[4].substring(28));
   const isFalse = parseInt(lines[5].substring(29));
-  monkeys.push({items, operation, test, isTrue, isFalse, inspectCount:0, orgItems: items.slice()});
+  monkeys.push({operation, test, isTrue, isFalse, inspectCount:0, orgItems});
 },'input.txt')
 
 const base = monkeys.reduce((prev, monkey) => prev * monkey.test, 1);
 
 function calculate(item, operation, useDivisor) {
-  let [old, oper, value] = operation.split(' ');
-  if (value === 'old') {
-    value = item;
-  } else {
-    value = parseInt(value);
-  }
-  const newValue = (oper === '+') ? item + value : item * value;
+  let [,op,value] = operation.split(' ');
+  value = (value === 'old') ? item : parseInt(value);
+  const newValue = (op === '+') ? item + value : item * value;
   return useDivisor ? Math.floor(newValue / 3) : (newValue % base);
 }
 
@@ -29,19 +25,16 @@ const checkWorry = (roundsMax, useDivisor) => {
     monkey.items = monkey.orgItems.slice();
   });
   for (let rounds = 0; rounds < roundsMax; rounds++) {
-    for (let monkeyIndex = 0; monkeyIndex < monkeys.length; monkeyIndex++) {
-      const monkey = monkeys[monkeyIndex];
+    monkeys.forEach(monkey => {
       while (monkey.items.length > 0) {
-        const item = monkey.items.shift();
         monkey.inspectCount++;
-        const newValue = calculate(item, monkey.operation, useDivisor);
-        let test = (newValue % monkey.test) === 0;
-        monkeys[test ? monkey.isTrue: monkey.isFalse].items.push(newValue);
+        const newValue = calculate(monkey.items.shift(), monkey.operation, useDivisor);
+        monkeys[(newValue % monkey.test) === 0 ? monkey.isTrue: monkey.isFalse].items.push(newValue);
       }
-    }
+    });
   }
-  let data = monkeys.map(monkey => monkey.inspectCount).sort((a, b)=>b-a);
-  return data[0] * data[1]
+  let result = monkeys.map(monkey => monkey.inspectCount).sort((a, b)=>b-a);
+  return result[0] * result[1];
 }
 
 let answer1 = checkWorry(20, true), answer2 = checkWorry(10000, false);
