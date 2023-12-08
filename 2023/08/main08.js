8627034cebbf6d0cd8521ef226af71dc36771083
hebol <martin@sjoblom.se>
2023-12-08 07:04:55 +0100

    Some cleanup day 8

--- 2023/08/main08.js
+++ 2023/08/main08.js
@@ -4,28 +4,23 @@ let arrayList = utils.readFile('input.txt')
 const directions = arrayList[0].trim().split('');
 const map = arrayList.slice(1).reduce((rest, line) => {
   const match = line.match( /(.*) = \((.*), (.*)\)/);
-  rest[match[1]] = {
-    left: match[2],
-    right: match[3]
-  };
+  rest[match[1]] = { left: match[2], right: match[3]};
   return rest;
 }, {});
 
-function travelPath(startList) {
-  let current = startList;
+function travelPath(pos) {
   let steps = 0;
-  while (current.filter(c => c[2] !== 'Z').length > 0) {
+  while (pos[2] !== 'Z') {
     const direction = directions[steps++ % directions.length];
-    current = current.map(aC => (direction === 'R') ? map[aC].right : map[aC].left);
+    pos = direction === 'R' ? map[pos].right : map[pos].left;
   }
-  return {steps, current};
+  return {steps, pos};
 }
 
-let answer1 = travelPath(['AAA']).steps;
+let answer1 = travelPath('AAA').steps;
 
 const startList = Object.keys(map).filter(key => key[2] === 'A');
-const steps = startList.map(start => travelPath([start]));
-let answer2 = utils.minstaGemensammaNamnaren(steps.map(({steps}) => steps));
+const answer2 = utils.minstaGemensammaNamnaren(startList.map(start => travelPath(start).steps));
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1: 20569 Answer2: 21366921060721
