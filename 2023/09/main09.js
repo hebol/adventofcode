ae1828990a5a2d5dcd6fdbb16dd862adce2646c9
hebol <martin@sjoblom.se>
2023-12-09 08:27:46 +0100

    Ready and prepare for tomorrow

--- 2023/09/main09.js
+++ 2023/09/main09.js
@@ -1,9 +1,28 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
 
-let answer1 = -1;
-let answer2 = -1;
+let arrayList = utils.processLine(line => {
+  const numbers = line.split(' ').map(Number);
+  const results = [numbers];
+  while (results[results.length - 1].find(n => n !== 0)) {
+    const row = results[results.length - 1];
+    const next = [];
+    for (let i = 0; i < row.length - 1; i++) {
+      next.push(row[i + 1] - row[i]);
+    }
+    results.push(next);
+  }
+  for (let i = results.length - 1 ; i >= 0 ; i--) {
+    let oldLast = i < results.length - 1 ? results[i + 1][results[i + 1].length - 1] : 0;
+    let oldFirst = i < results.length - 1 ? results[i + 1][0] : 0;
+    results[i].push( results[i][results[i].length - 1] + oldLast);
+    results[i].unshift( results[i][0] - oldFirst);
+  }
+
+  return {first: results[0][0], last: results[0][results[0].length - 1]};
+}, 'input.txt');
+
+let answer1 = utils.sumArray(arrayList.map(({first}) => first));
+let answer2 = utils.sumArray(arrayList.map(({last}) => last));
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
-// Answer1:  Answer2:
+// Answer1: 1129 Answer2: 1934898178
