459dc4956491dcc7a223063924070a28557138ed
hebol <martin@sjoblom.se>
2023-12-15 10:24:27 +0100

    Cleanup

--- 2023/14/main14.js
+++ 2023/14/main14.js
@@ -1,15 +1,15 @@
 const utils = require('../../utils');
-const inputs = utils.readFile('input.txt');
-function tiltTo(map, dir) {
+const inputs = utils.readFile('input.txt').map(row => row.split(''));
+function tiltTo(map, dir, doCalc) {
   let load = 0;
-  for (let y = (dir == "north" ? 0 : map.length - 1); (dir == "north" ? y < map.length : y >= 0); (dir == "north" ? y++ : y--)) {
-    for (let x = (dir == "west" ? 0 : map[y].length - 1); (dir == "west" ? x < map[y].length : x >= 0);(dir == "west" ? x++ : x--)) {
+  for (let y = (dir === "north" ? 0 : map.length - 1); (dir === "north" ? y < map.length : y >= 0); (dir === "north" ? y++ : y--)) {
+    for (let x = (dir === "west" ? 0 : map[y].length - 1); (dir === "west" ? x < map[y].length : x >= 0);(dir === "west" ? x++ : x--)) {
       function move(x, y) {
         let [toX, toY] = { "north": [x, y-1], "south": [x, y+1], "west": [x-1, y], "east": [x+1, y]}[dir];
 
         if (toY >= 0 && toY < map.length && toX >= 0 && toX < map[y].length && map[y][x] === "O" && map[toY][toX] === ".") {
-          map[y]   = map[y].substring(0, x) +     "." + map[y].substring(x + 1);
-          map[toY] = map[toY].substring(0, toX) + "O" + map[toY].substring(toX + 1);
+          map[y][x] = '.';
+          map[toY][toX] = 'O';
           move(toX, toY);
         }
       }
@@ -17,44 +17,43 @@ function tiltTo(map, dir) {
     }
   }
 
-  if (calcPart1 || dir == "east")
-    for (let i = 0, multiplier = 1; i < map.length; i++, multiplier++) {
-      load += (map[map.length - 1 - i].match(/O/g) || []).length * multiplier;
+  if (doCalc) {
+    for (let i = 0 ; i < map.length; i++) {
+      load += utils.sumArray(map[map.length - 1 - i].map((c) => c === 'O' ? (i+1): 0));
     }
+  }
+
   return [map, load];
 }
 
-let pattern = [];
-let calcPart1 = true;
-let answer1 = tiltTo(inputs, "north")[1];
+let answer1 = tiltTo(inputs, "north", true)[1];
 
-calcPart1 = false;
-let northState, westState, southState;
-let answer;
+const runPart2 = () => {
+  let pattern = [];
+  let state = [inputs];
 
-let eastState = [inputs];
+  while (true) {
+    state = tiltTo(state[0], "north");
+    state = tiltTo(state[0], "west");
+    state = tiltTo(state[0], "south");
+    state = tiltTo(state[0], "east", true);
 
-while (true) {
-  northState = tiltTo(eastState[0], "north");
-  westState  = tiltTo(northState[0], "west");
-  southState = tiltTo(westState[0], "south");
-  eastState  = tiltTo(southState[0], "east");
+    let aMap = state[0].join("\n") + "_" + state[1].toString();
 
-  let aMap = eastState[0].join("\n") + "_" + eastState[1].toString();
+    if (pattern.includes(aMap)) {
+      const repeatsFrom = pattern.indexOf(aMap);
+      const patternLength =  pattern.length;
+      let cycleLength = patternLength - repeatsFrom;
+      let modulo = (999999999 - repeatsFrom) % cycleLength;
 
-  if (pattern.includes(aMap)) {
-    answer = [pattern.indexOf(aMap), pattern.length];
-    break;
-  } else {
-    pattern.push(aMap);
+      return pattern[repeatsFrom + modulo].split("_")[1];
+    } else {
+      pattern.push(aMap);
+    }
   }
 }
 
-let repeatsFrom = answer[0];
-let cycleLength = answer[1] - repeatsFrom;
-let modulo = (999999999 - repeatsFrom) % cycleLength;
-
-let answer2 = pattern[repeatsFrom + modulo].split("_")[1];
+let answer2 = runPart2();
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 //Answer1: 109424 Answer2: 102509
