be7e3acc5d6440bc5ed54069d4f2ef385ce3e2c3
Martin Sjöblom <martin@sjoblom.se>
2023-12-19 19:15:16 +0100

    Cleanup

--- 2023/20/main20.js
+++ 2023/20/main20.js
@@ -1,8 +1,9 @@
 const utils = require('../../utils');
-let arrayList = utils.readFile('test.txt')
+//let arrayList = utils.readFile('test.txt')
 utils.checkDataInputFileExists();
 
-let answer1 = -1, answer2 = -1;
+let answer1 = -1;
+let answer2 = -1;
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1:  Answer2:
