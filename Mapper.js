428f99c40bab616113b2b3c3406696592e69a61b
hebol <martin@sjoblom.se>
2024-12-04 10:19:34 +0100

    Prettier map!

--- Mapper.js
+++ Mapper.js
@@ -2,9 +2,16 @@ const utils = require('./utils');
 
 class Mapper {
   constructor(filename) {
-    this.map = utils.readFile(filename).map(line => line.split(''));
+    this.map = filename ? utils.readFile(filename).map(line => line.split('')) : [[]];
     this.width = this.map[0].length;
     this.height = this.map.length;
+  }
+  static byDimensions(width, height, c = '.') {
+    const map = new Mapper();
+    map.map = Array(height).fill().map(() => Array(width).fill(c));
+    map.width = width;
+    map.height = height;
+    return map;
   }
   get(x, y) {
     if (x < 0 || x >= this.width || y < 0 || y >= this.height) {
@@ -20,18 +27,36 @@ class Mapper {
     this.map[y][x] = c;
     return oldC;
   }
-  print (title, aSecondMap)  {
+  print(title, aSecondMap) {
     const aMap = aSecondMap || this.map;
-    title && console.log(title);
-    if (typeof aMap[0] === 'string') {
-      console.log(aMap.join('\n'));
-    } else {
-      console.log(aMap.map(row => row.join('')).join('\n'));
+    const contentWidth = this.width;
+    const titleWidth = title ? title.length : 0;
+    const totalWidth = Math.max(contentWidth, titleWidth);
+
+    if (title) {
+      const padding = Math.floor((totalWidth - titleWidth) / 2);
+      console.log(' '.repeat(padding) + title + ' '.repeat(totalWidth - titleWidth - padding));
     }
+
+    const rowPadding = ' '.repeat(Math.floor((totalWidth - contentWidth) / 2));
+    const horizontalBorder =  rowPadding + '+' + '-'.repeat(contentWidth) + '+';
+    console.log(horizontalBorder);
+
+    if (typeof aMap[0] === 'string') {
+      aMap.forEach(row => {
+        console.log(rowPadding + '|' + row + '|');
+      });
+    } else {
+      aMap.forEach(row => {
+        console.log(rowPadding + '|' + row.join('') + '|');
+      });
+    }
+
+    console.log(horizontalBorder);
     console.log('\n');
   }
 
-findMultiple( aFun) {
+  findMultiple( aFun) {
     let result = [];
     for (let x = 0; x < this.width; x++) {
       for (let y = 0; y < this.height; y++) {
@@ -54,3 +79,9 @@ findMultiple( aFun) {
 }
 
 module.exports = Mapper;
+
+if (__filename === require.main.filename) {
+  const map = Mapper.byDimensions(3, 3, '.');
+  map.set(1, 1, 'X');
+  map.print("Small test");
+}
