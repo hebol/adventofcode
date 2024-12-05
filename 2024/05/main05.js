5bc78fc9918b842a29f4da7e46e651c2b9d08851
hebol <martin@sjoblom.se>
2024-12-05 18:33:00 +0100

    Day 5 ready

--- 2024/05/main05.js
+++ 2024/05/main05.js
@@ -1,5 +1,5 @@
 const utils = require('../../utils');
-let arrayList = utils.readFile('test.txt')
+let arrayList = utils.readFile('input.txt')
 
 const rules = arrayList.filter(s => s.indexOf('|') >= 0).map(s => s.split('|').map(s=>parseInt(s)));
 const tc = arrayList.filter(s => s.indexOf('|') < 0).map(s => s.split(',').map(s=>parseInt(s)));
@@ -15,24 +15,16 @@ const ok = tc.filter(t => {
     }
   }) === undefined;
 });
-const error = tc.filter(t => {
-  return rules.find(([first, second]) => {
-    const i1 = t.indexOf(first);
-    const i2 = t.indexOf(second);
-    if (i1 >= 0 && i2 >= 0) {
-      return i1 > i2;
-    } else {
-      return false;
-    }
-  }) === undefined;
-});
+const error = tc.filter(t => ok.indexOf(t) < 0);
 
-
-
-console.log(error)
+let answer2 = error.reduce((total, update) => {
+    const sortedUpdate = [...update].sort((p1,p2) => {
+      return rules.some(rule => rule[0] === p1 && rule[1] === p2) ? -1 : 1
+    })
+    return total + sortedUpdate[Math.floor(update.length / 2)]
+  }, 0)
 
 let answer1 = utils.sumArray(ok.map(a => a[Math.floor(a.length / 2)]));
-let answer2 = -1;
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1:  Answer2:
