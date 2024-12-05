7afd891d0ec4165ee95b0902fd1617cde7838858
hebol <martin@sjoblom.se>
2024-12-05 06:22:57 +0100

    part solution

--- 2024/05/main05.js
+++ 2024/05/main05.js
@@ -1,8 +1,37 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let arrayList = utils.readFile('test.txt')
 
-let answer1 = -1;
+const rules = arrayList.filter(s => s.indexOf('|') >= 0).map(s => s.split('|').map(s=>parseInt(s)));
+const tc = arrayList.filter(s => s.indexOf('|') < 0).map(s => s.split(',').map(s=>parseInt(s)));
+
+const ok = tc.filter(t => {
+  return rules.find(([first, second]) => {
+    const i1 = t.indexOf(first);
+    const i2 = t.indexOf(second);
+    if (i1 >= 0 && i2 >= 0) {
+      return i1 > i2;
+    } else {
+      return false;
+    }
+  }) === undefined;
+});
+const error = tc.filter(t => {
+  return rules.find(([first, second]) => {
+    const i1 = t.indexOf(first);
+    const i2 = t.indexOf(second);
+    if (i1 >= 0 && i2 >= 0) {
+      return i1 > i2;
+    } else {
+      return false;
+    }
+  }) === undefined;
+});
+
+
+
+console.log(error)
+
+let answer1 = utils.sumArray(ok.map(a => a[Math.floor(a.length / 2)]));
 let answer2 = -1;
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
