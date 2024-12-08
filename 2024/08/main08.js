137269443b03a4f1b3ee5469d7ba5ee1ab82d971
hebol <martin@sjoblom.se>
2024-12-08 07:23:22 +0100

    ready and prepare for next

--- 2024/08/main08.js
+++ 2024/08/main08.js
@@ -1,9 +1,41 @@
-const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+const Mapper = require("../../Mapper");
+let map = new Mapper( 'input.txt');
 
-let answer1 = -1;
-let answer2 = -1;
+const antennas = {};
+
+map.findMultiple((c, x, y) => {
+  if (c !== '.') {
+    const array = antennas[c] || [];
+    array.push({x, y});
+    antennas[c] = array;
+  }
+});
+
+const calculateEchos = allowMultiple => {
+  let echos = {};
+
+  Object.keys(antennas).forEach(c => {
+    const array = antennas[c];
+    array.forEach(({x, y}) => {
+      array.filter(({x: x2, y: y2}) => x !== x2 || y !== y2).forEach(({x: x2, y: y2}) => {
+        const dx = x - x2;
+        const dy = y - y2;
+        let c = allowMultiple ? 0 : 1;
+        while (map.isValidPos(x + c * dx, y + c * dy)) {
+          echos[(x + c * dx) + ',' + (y + c * dy)] = true;
+          if ( !allowMultiple) {
+            break;
+          }
+          c++;
+        }
+      })
+    });
+  });
+  return Object.keys(echos).length;
+}
+
+let answer1 = calculateEchos(false);
+let answer2 = calculateEchos(true);
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
-// Answer1:  Answer2:
+// Answer1: 311 Answer2: 1115
