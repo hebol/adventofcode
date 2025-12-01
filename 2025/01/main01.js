5b4c2402018a3f9d8bb006e8583caa7829a22342
hebol <martin@sjoblom.se>
2025-12-01 09:49:34 +0100

    First day ready

--- 2025/01/main01.js
+++ 2025/01/main01.js
@@ -1,9 +1,37 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let answer2 = 0;
 
-let answer1 = -1;
-let answer2 = -1;
+const array = utils.readFile('input.txt').map(item => {
+  let result = {dir: item[0], count: parseInt(item.substring(1), 10)};
+  if (result.count > 100) {
+    let fullRevolts = Math.floor(result.count / 100);
+    answer2 += fullRevolts;
+    result.count = result.count % 100;
+  }
+  return result;
+});
+
+let answer1 = 0;
+
+array.reduce((acc, item) => {
+  let result = (acc + (item.dir === 'L' ? -item.count : item.count));
+
+  if ((result < 0) || result > 99) {
+    if (acc !== 0) {
+      answer2++;
+    }
+    result = result < 0 ? result + 100 : result - 100;
+  } else {
+    if (result === 0) {
+      answer2++;
+    }
+  }
+  if (result === 0) {
+    answer1++;
+  }
+  return result;
+}, 50)
+
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
-// Answer1:  Answer2:
+// Answer1: 1026 Answer2: 5923
