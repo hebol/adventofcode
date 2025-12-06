e0db035423aa250a614d0fb7d41eeb432255897f
hebol <martin@sjoblom.se>
2025-12-06 09:37:13 +0100

    Day 6 ready but no cleanup

--- 2025/03/main03.js
+++ 2025/03/main03.js
@@ -1,22 +1,18 @@
 const utils = require('../../utils');
-let arrayList = utils.readFile('input.txt')
+let arrayList = utils.readFile('input.txt').map(line => line.split('').map(Number));
 
-console.log(arrayList)
-
-const answer1 = utils.sumArray(arrayList.map(array => {
-  const line = array.split('').map(Number);
-  const max = Math.max(...line);
+const findHighest = (line, len) => {
+  const subline = line.slice(0, line.length - len);
+  const max = Math.max(...subline);
   const foundAt = line.indexOf(max);
-  if (foundAt === line.length - 1) {
-    const max2 = Math.max(...line.slice(0, line.length - 1));
-    return max2 * 10 + max;
-  } else {
-    const max2 = Math.max(...line.slice(foundAt + 1, line.length));
-    return max * 10 + max2;
-  }
-}))
+  let rest = len > 0 ? findHighest(line.slice(foundAt + 1), len - 1) : 0;
+  let high = max * Math.pow(10, len);
+  return high + rest;
+}
 
-let answer2 = -1;
+const answer1 = utils.sumArray(arrayList.map(arr => findHighest(arr, 1)));
+
+let answer2 = utils.sumArray(arrayList.map(arr => findHighest(arr, 11)));
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1: 16887 Answer2:
--- 2025/06/main06.js
+++ 2025/06/main06.js
@@ -1,9 +1,49 @@
 const utils = require('../../utils');
-//let arrayList = utils.readFile('test.txt')
-utils.checkDataInputFileExists();
+let arrayList = utils.readFile('input.txt').map(line => line.trim().split(/ +/));
+let operators = arrayList.pop();
+arrayList = arrayList.map(array => array.map(Number));
 
-let answer1 = -1;
-let answer2 = -1;
+let answer1 = 0;
+
+while (arrayList[0].length > 0) {
+  const vals = arrayList.map(arr => arr.pop());
+  const op = operators.pop();
+  let result = vals.reduce((acc, val) => {
+    return op === '+' ? acc + val : acc * val;
+  }, op === '+' ? 0 : 1);
+  //console.log({vals, op, result});
+  answer1 += result;
+}
+
+let arrayList2 = utils.readFileNoFilter('input.txt');
+operators = arrayList2.pop().trim().split(/ +/);
+
+const numbers = [];
+let subArray = [];
+while (arrayList2[0].length > 0) {
+  let value = arrayList2.map(line => line.slice(-1)).join('').trim();
+  arrayList2 = arrayList2.map(line => line.slice(0, -1));
+  if (value.length) {
+    subArray.push(Number(value));
+  } else {
+    numbers.push(subArray);
+    subArray = [];
+  }
+}
+if (subArray.length) {
+  numbers.push(subArray);
+}
+
+let answer2 = 0;
+while (numbers.length > 0) {
+  const vals = numbers.pop();
+  const op = operators.shift();
+  let result = vals.reduce((acc, val) => {
+    return op === '+' ? acc + val : acc * val;
+  }, op === '+' ? 0 : 1);
+  answer2 += result;
+}
+
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
-// Answer1:  Answer2:
+// Answer1: 6503327062445 Answer2: 9640641878593
