f8f887f4bf760f4a25e55e6df18647e1c71db8f7
hebol <martin@sjoblom.se>
2025-12-06 09:43:49 +0100

    Some cleaning

--- 2025/06/main06.js
+++ 2025/06/main06.js
@@ -5,14 +5,12 @@ arrayList = arrayList.map(array => array.map(Number));
 
 let answer1 = 0;
 
+const processGroup = (vals, op) => {
+  return vals.reduce((acc, val) => op === '+' ? acc + val : acc * val, op === '+' ? 0 : 1);
+}
+
 while (arrayList[0].length > 0) {
-  const vals = arrayList.map(arr => arr.pop());
-  const op = operators.pop();
-  let result = vals.reduce((acc, val) => {
-    return op === '+' ? acc + val : acc * val;
-  }, op === '+' ? 0 : 1);
-  //console.log({vals, op, result});
-  answer1 += result;
+  answer1 += processGroup(arrayList.map(arr => arr.pop()), operators.pop());
 }
 
 let arrayList2 = utils.readFileNoFilter('input.txt');
@@ -36,14 +34,8 @@ if (subArray.length) {
 
 let answer2 = 0;
 while (numbers.length > 0) {
-  const vals = numbers.pop();
-  const op = operators.shift();
-  let result = vals.reduce((acc, val) => {
-    return op === '+' ? acc + val : acc * val;
-  }, op === '+' ? 0 : 1);
-  answer2 += result;
+  answer2 += processGroup(numbers.pop(), operators.shift());
 }
-
 
 console.log("Answer1:", answer1, "Answer2:", answer2);
 // Answer1: 6503327062445 Answer2: 9640641878593
